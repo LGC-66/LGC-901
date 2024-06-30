8265eeff4f3ef36aea07cbd957b4b27fc92c2d0e
LGC-66 <ozzy.osbourne13@outlook.com>
2024-06-29 21:46:57 -0600

    Add files via upload

---
+++ Demo23/inventory/src/main/java/com/univer/inventory/entity/ProductoEntity.java
@@ -0,0 +1,55 @@
+package com.univer.inventory.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "PRODUCTO")
+public class ProductoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private int id;
+
+    @Column(name = "NAME")
+    private String nombre;
+
+    @Column(name = "SKU")
+    private String sku;
+
+    @Column(name = "CANTIDAD")
+    private Integer cantidad;
+
+    // Getters y Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+}
